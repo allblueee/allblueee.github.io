ffc61e137addc7e3a216fc6f2c35961f73fbffe5
allblueee <allblueee798@gmail.com>
2022-05-18 22:30:50 +0800

    测试

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,20 @@
+const { defaultTheme } = require('vuepress')
+
 module.exports = {
     lang: 'zh-CN',
-    title: '你好， VuePress ！',
+    title: 'allblueee',
     description: '这是我的第一个 VuePress 站点',
+    theme: defaultTheme({
+        // 默认主题配置
+        navbar: [
+            {
+                text: '首页',
+                link: '/',
+            },
+            {
+                text:'算法',
+                link:'/index.md'
+            }
+        ],
+    }),
 }
